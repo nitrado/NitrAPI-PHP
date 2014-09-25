58f420ca7ad0eb71aef7e9cc3dc044e1f661dfe9
Alexander Birkner <alex.birkner@gmail.com>
2014-09-25 19:46:06 +0200

    added missing files

---
+++ examples/get_services.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+try {
+    $api = new \Nitrapi\Nitrapi("NTc2OGMyMjg0NGMxMGY5MDVhOTE1ZjJmY2VjODY2NDIzOWJiMGYzZmU0ZTI1MWMwNmQzMDZhMGYzNGZmYjU4Yw");
+
+    /**
+     * @var $service \Nitrapi\Services\Service
+     */
+
+    foreach ($api->getServices() as $service) {
+        var_dump($service->getSuspendDate());
+    }
+
+} catch(\Exception $e) {
+    var_dump("Error: " . $e->getMessage());
+}
