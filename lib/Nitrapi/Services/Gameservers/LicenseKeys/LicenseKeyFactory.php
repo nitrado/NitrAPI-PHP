7ee312a6294939ea7519bd5f8f0c5152753a6229
Alexander Birkner <alex.birkner@gmail.com>
2014-10-16 16:25:11 +0200

    #readme file change + added license keys

---
+++ lib/Nitrapi/Services/Gameservers/LicenseKeys/LicenseKeyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nitrapi\Services\Gameservers\LicenseKeys;
+
+use Nitrapi\Services\Gameservers\Gameserver;
+
+class LicenseKeyFactory
+{
+    public static function factory(Gameserver $service, array $data) {
+        return new LicenseKey($service, $data);
+    }
+}
