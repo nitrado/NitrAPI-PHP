62e0a8a9c2abd4627a3a2a4a205d6ea1df6784bc
Alexander Birkner <alex.birkner@gmail.com>
2015-08-17 18:09:00 +0200

    + added chunkfix api command

--- lib/Nitrapi/Services/Gameservers/Games/Minecraft.php
+++ lib/Nitrapi/Services/Gameservers/Games/Minecraft.php
@@ -5,4 +5,14 @@ namespace Nitrapi\Services\Gameservers\Games;
 class Minecraft extends Game
 {
     protected $game = "minecraft";
+
+    public function startChunkfix($world, $limit = 0) {
+        $url = "services/" . $this->service->getId() . "/gameservers/games/minecraft/chunkfix";
+        $this->service->getApi()->dataPost($url, [
+            'world' => $world,
+            'limit' => $limit,
+        ]);
+
+        return true;
+    }
 }
