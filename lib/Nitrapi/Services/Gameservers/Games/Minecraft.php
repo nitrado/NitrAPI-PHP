1accc400b0433ca8b13162687ea7593d7031ed55
Alexander Birkner <alex.birkner@gmail.com>
2015-08-18 14:36:00 +0200

    + added mcmyadmin settings + added bungeecord settings

--- lib/Nitrapi/Services/Gameservers/Games/Minecraft.php
+++ lib/Nitrapi/Services/Gameservers/Games/Minecraft.php
@@ -54,4 +54,48 @@ class Minecraft extends Game
 
         return true;
     }
+
+    /**
+     * Changing bungeecord settings
+     *
+     * @param bool|false $enabled
+     * @param bool|false $only
+     * @param string $firewall
+     * @param null $ip
+     * @return bool
+     */
+    public function setBungeeCord($enabled = false, $only = false, $firewall = 'off', $ip = null) {
+        $url = "services/" . $this->service->getId() . "/gameservers/games/minecraft/bungeecord";
+        $this->service->getApi()->dataPost($url, [
+            'enabled' => (int)$enabled,
+            'only' => (int)$only,
+            'firewall' => $firewall,
+            'firewall_ip' => $ip,
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Changing mcmyadmin settings
+     *
+     * @param bool|false $enabled
+     * @param null $username
+     * @param null $password
+     * @param null $language
+     * @return bool
+     */
+    public function setMcMyAdmin($enabled = false, $username = null, $password = null, $language = null) {
+        $url = "services/" . $this->service->getId() . "/gameservers/games/minecraft/mcmyadmin";
+        $this->service->getApi()->dataPost($url, [
+            'enabled' => (int)$enabled,
+            'username' => $username,
+            'password' => $password,
+            'language' => $language,
+        ]);
+
+        return true;
+    }
+
+
 }
