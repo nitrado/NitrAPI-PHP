c0a7323b84eb8cee63b2f94b849f2f5f792c1237
Alexander Birkner <alexander.birkner@marbis.net>
2015-12-06 12:23:03 +0100

    Update Sun Dec  6 12:23:03 CET 2015

---
+++ lib/Nitrapi/Admin/Servers/Ipmi/Ipmi.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Nitrapi\Admin\Servers\IPMI;
+
+use Nitrapi\Admin\Servers\ServerManager;
+
+class Ipmi
+{
+    protected $serverManager;
+
+    public function __construct(ServerManager $serverManager) {
+        $this->setServerManager($serverManager);
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIKVM($hostname) {
+        return $this->getServerManager()->getAdmin()->getApi()->dataGet('/admin/server/ipmi/ikvm', null, [
+            'query' => [
+                'hostname' => $hostname
+            ]
+        ])['ipmi'];
+    }
+
+    public function doReset($hostname) {
+        $this->getServerManager()->getAdmin()->getApi()->dataPost('/admin/server/ipmi/reset', [
+            'hostname' => $hostname
+        ]);
+        return true;
+    }
+
+    public function doPowerOff($hostname) {
+        $this->getServerManager()->getAdmin()->getApi()->dataPost('/admin/server/ipmi/power_off', [
+            'hostname' => $hostname
+        ]);
+        return true;
+    }
+
+    public function doPowerOn($hostname) {
+        $this->getServerManager()->getAdmin()->getApi()->dataPost('/admin/server/ipmi/power_on', [
+            'hostname' => $hostname
+        ]);
+        return true;
+    }
+
+    public function doIKVMReset($hostname) {
+        $this->getServerManager()->getAdmin()->getApi()->dataPost('/admin/server/ipmi/ikvm_reset', [
+            'hostname' => $hostname
+        ]);
+        return true;
+    }
+
+    /**
+     * @param ServerManager $serverManager
+     */
+    protected function setServerManager(ServerManager $serverManager) {
+        $this->serverManager = $serverManager;
+    }
+
+    /**
+     * @return ServerManager
+     */
+    public function getServerManager() {
+        return $this->serverManager;
+    }
+}
