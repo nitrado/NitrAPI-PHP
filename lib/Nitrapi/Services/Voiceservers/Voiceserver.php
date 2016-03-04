5c539ed7754c42e89884ee7eb8e426bb38aafc2f
Alexander Birkner <alexander.birkner@marbis.net>
2016-03-04 12:40:59 +0100

    Update Fri Mar  4 12:40:59 CET 2016

--- lib/Nitrapi/Services/Voiceservers/Voiceserver.php
+++ lib/Nitrapi/Services/Voiceservers/Voiceserver.php
@@ -58,7 +58,99 @@ class Voiceserver extends Service
     public function doReinstall() {
         $url = "services/" . $this->getId() . "/voiceservers/reinstall";
         $this->getApi()->dataPost($url);
+
+        return true;
+    }
+
+    /**
+     * Configures the voiceserver
+     *
+     * @return bool
+     */
+    public function doConfigChange($key, $value) {
+        $url = "services/" . $this->getId() . "/voiceservers";
+        $this->getApi()->dataPost($url, [
+            'key' => $key,
+            'value' => $value
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Returns all available Backups
+     *
+     * @return array
+     */
+    public function getBackups() {
+        if (!isset($this->info['voiceserver']['specific']['snapshots'])) {
+            return [];
+        }
+
+        return $this->info['voiceserver']['specific']['snapshots'];
+    }
+
+    /**
+     * Creates a new Backup
+     *
+     * @return array
+     */
+    public function createBackup() {
+        $url = "services/" . $this->getId() . "/voiceservers/backup";
+        $result = $this->getApi()->dataPost($url)['snapshot'];
+
+        $this->refresh();
+        return $result;
+    }
+
+    /**
+     * Deployes a specific snapshot to the Voiceserver
+     * The server will be restarted after a successful deployment.
+     *
+     * @param $id
+     * @return bool
+     */
+    public function restoreBackup($id) {
+        $url = "services/" . $this->getId() . "/voiceservers/backup/".(int)$id."/restore";
+        $this->getApi()->dataPost($url);
+
+        return true;
+    }
+
+    /**
+     * Deletes a specific Backup from the Voiceserver
+     *
+     * @param $id
+     * @return bool
+     */
+    public function deleteBackup($id) {
+        $url = "services/" . $this->getId() . "/voiceservers/backup/".(int)$id;
+        $this->getApi()->dataDelete($url);
+
         $this->refresh();
         return true;
+    }
+
+    /**
+     * Downloads a specific backup file
+     *
+     * @param $id
+     * @return string
+     */
+    public function downloadBackup($id) {
+        $url = "services/" . $this->getId() . "/voiceservers/backup/".(int)$id;
+        $backup = $this->getApi()->dataGet($url)['snapshot'];
+
+        return base64_decode($backup);
+    }
+
+    public function uploadBackup($backup) {
+        $url = "services/" . $this->getId() . "/voiceservers/backup/upload/";
+        $result = $this->getApi()->dataPost($url, null, null, [
+            'body' => base64_encode($backup)
+        ])['snapshot'];
+
+        $this->refresh();
+        return $result;
     }
 }
