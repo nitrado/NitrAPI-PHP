919cbcf8c3c5a1f9c041c6e1cea870c8a1dd54cf
Alexander Birkner <alexander.birkner@marbis.net>
2016-09-17 19:55:12 +0200

    API-178: Added Cloud Server Product

---
+++ lib/Nitrapi/Order/Pricing/Products/CloudServer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nitrapi\Order\Pricing\Products;
+
+use Nitrapi\Order\Pricing\PartPricing;
+
+class CloudServer extends PartPricing  {
+
+    protected static $product = 'cloud_server';
+}
