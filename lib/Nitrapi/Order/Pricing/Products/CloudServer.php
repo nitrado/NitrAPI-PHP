69ea37ca116b0229e86cec28ba8fe22411bde0fd
Alexander Birkner <alexander.birkner@marbis.net>
2016-09-17 20:53:58 +0200

    API-178: Added image set method

--- lib/Nitrapi/Order/Pricing/Products/CloudServer.php
+++ lib/Nitrapi/Order/Pricing/Products/CloudServer.php
@@ -2,9 +2,16 @@
 
 namespace Nitrapi\Order\Pricing\Products;
 
+use Nitrapi\Common\Exceptions\NitrapiErrorException;
 use Nitrapi\Order\Pricing\PartPricing;
+use Nitrapi\Services\CloudServers\Image;
 
 class CloudServer extends PartPricing  {
 
     protected static $product = 'cloud_server';
+
+    public function setImage(Image $image) {
+        $this->additionals['image_id'] = $image->getId();
+    }
+
 }
