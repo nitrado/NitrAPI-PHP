06f1878c8d387dc761c716e0926aabc7853e9b32
Stefan Reusch <kenterfie@googlemail.com>
2016-11-08 14:49:31 +0100

    added dynamic share api calls

---
+++ lib/Nitrapi/DynamicShare/DynamicShare.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Nitrapi\DynamicShare;
+
+use Nitrapi\Nitrapi;
+
+class DynamicShare {
+    protected $api;
+
+    public function __construct(Nitrapi $api) {
+        $this->setApi($api);
+    }
+
+    /**
+     * @param string $pattern
+     * @param int $service_id
+     * @return string token
+     */
+    public function create($pattern = null, $service_id = 0) {
+        $url = "dynamic_share";
+        return $this->getApi()->dataPost($url, [
+            "pattern" => $pattern,
+            "service_id" => $service_id
+        ])['share']['token'];
+    }
+
+    public function share($token, $service_id = 0) {
+        $url = "dynamic_share";
+        return $this->getApi()->dataGet($url, null, [
+            'query' => [
+                "token" => $token,
+                "service_id" => $service_id
+            ]
+        ])['share'];
+    }
+
+    /**
+     * @param Nitrapi $api
+     */
+    protected function setApi(Nitrapi $api) {
+        $this->api = $api;
+    }
+
+    /**
+     * @return Nitrapi
+     */
+    public function getApi() {
+        return $this->api;
+    }
+}
