a7652a5033c7636a682e11dc6c0870ab979e8c3c
andre.becker <andre.becker@marbis.net>
2017-06-16 11:54:34 +0200

    Allow passing of "expires_in" parameter to token/sub

--- lib/Nitrapi/Admin/Admin.php
+++ lib/Nitrapi/Admin/Admin.php
@@ -19,7 +19,20 @@ class Admin extends NitrapiObject
         return new ServerManager($this);
     }
 
-    public function getUserToken($userId, $scopes, $serviceId = null) {
+    /**
+     * Requests a sub token for another user. This is required for calls that access the user's services.
+     *
+     * @param $userId The user to generate a token for
+     * @param array|string $scopes The scopes for the new token. Can only contain scopes of the current token.
+     * May be passed in as array or space separated string.
+     * @param null|integer $serviceId A serviceID to pin the token to. It won't be possible to access other services with this
+     * token. Passing null allows all services to be accessed.
+     * @param null|integer $expires_in The time in seconds the new token will be valid for. The life time of a sub token
+     * can never exceed the life time of the parent token. Pass in null for taking the parent token's life time.
+     *
+     * @return AccessToken
+     */
+    public function getUserToken($userId, $scopes, $serviceId = null, $expires_in = null) {
         $scopeString = $scopes;
         if (is_array($scopes)) {
             $scopeString = implode(' ', $scopes);
@@ -32,6 +45,10 @@ class Admin extends NitrapiObject
 
         if (!empty($serviceId)) {
             $payload['service_id'] = $serviceId;
+        }
+
+        if (!empty($expires_in)) {
+            $payload['expires_in'] = $expires_in;
         }
 
         return new AccessToken($this->getApi()->dataPost('token/sub', $payload)['token']);
