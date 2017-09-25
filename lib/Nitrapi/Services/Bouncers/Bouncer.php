219b82f31064659aa36ef74d2476dfd4ff128d0a
Aaron Fischer <mail@aaron-fischer.net>
2017-09-25 06:55:19 +0200

    Web 1482 bouncer endpoints (#32)
    * Add minimum requirements for apps
    
    Signed-off-by: Aaron Fischer <aaron.fischer@marbis.net>
    
    * WEB-1482: Add the IRC proxy endpoints

--- lib/Nitrapi/Services/Bouncers/Bouncer.php
+++ lib/Nitrapi/Services/Bouncers/Bouncer.php
@@ -5,9 +5,42 @@ namespace Nitrapi\Services\Bouncers;
 use Nitrapi\Nitrapi;
 use Nitrapi\Services\Service;
 
-class Bouncer extends Service
-{
+class Bouncer extends Service {
+    protected $info;
+
     public function __construct(Nitrapi $api, $id) {
         parent::__construct($api, $id);
+
+        if ($this->isActive()) {
+            $this->info = $this->getApi()->dataGet($this->url())['bouncer'];
+        }
+    }
+
+    public function getDetails() {
+        return new BouncerDetails($this->info);
+    }
+
+    public function addIdent($identName, $password) {
+        return $this->getApi()->dataPost($this->url(), [
+            'ident' => $identName,
+            'password' => $password
+        ]);
+    }
+
+    public function editPassword(Ident $ident, $newPassword) {
+        return $this->getApi()->dataPut($this->url(), [
+            'ident' => $ident->getIdent(),
+            'password' => $newPassword,
+        ]);
+    }
+
+    public function deleteIdent(Ident $ident) {
+        return $this->getApi()->dataDelete($this->url(), [
+            'ident' => $ident->getIdent()
+        ]);
+    }
+
+    private function url($path = '') {
+        return 'services/' . $this->getId() . '/bouncers' . $path;
     }
 }
