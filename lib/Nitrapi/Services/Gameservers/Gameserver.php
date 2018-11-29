b8468d396060626cb5ac29a1bbf0904564a54c7d
Alexander Birkner <alex.birkner@gmail.com>
2018-11-29 12:07:58 +0100

    API-649: Added Backup methods (#61)
    * API-649: Added Backup methods
    
    * Small PR fixes

--- lib/Nitrapi/Services/Gameservers/Gameserver.php
+++ lib/Nitrapi/Services/Gameservers/Gameserver.php
@@ -2,6 +2,7 @@
 
 namespace Nitrapi\Services\Gameservers;
 
+use Nitrapi\Common\Exceptions\NitrapiErrorException;
 use Nitrapi\Common\Exceptions\NitrapiHttpErrorException;
 use Nitrapi\Common\Exceptions\NitrapiServiceNotActiveException;
 use Nitrapi\Nitrapi;
@@ -123,7 +124,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function doRestart($message = null, $restartMessage = null) {
-        $url = "services/" . $this->getId() . "/gameservers/restart";
+        $url = 'services/' . $this->getId() . '/gameservers/restart';
         $this->getApi()->dataPost($url, array(
             'message' => $message,
             'restart_message' => $restartMessage,
@@ -139,7 +140,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function doStop($message = null, $stopMessage = null) {
-        $url = "services/" . $this->getId() . "/gameservers/stop";
+        $url = 'services/' . $this->getId() . '/gameservers/stop';
         $this->getApi()->dataPost($url, array(
             'message' => $message,
             'stop_message' => $stopMessage,
@@ -153,7 +154,7 @@ class Gameserver extends Service {
      * @return MariaDB
      */
     public function createDatabase($credentials = array(), $options = array()) {
-        $url = "services/" . $this->getId() . "/gameservers/mariadbs";
+        $url = 'services/' . $this->getId() . '/gameservers/mariadbs';
 
         $result = $this->getApi()->dataPost($url, $credentials, null, $options);
         return MariaDBFactory::factory($this, $result['database']['id']);
@@ -165,7 +166,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function getDatabases() {
-        $url = "services/" . $this->getId() . "/gameservers/mariadbs";
+        $url = 'services/' . $this->getId() . '/gameservers/mariadbs';
         $result = $this->getApi()->dataGet($url);
         return $result['databases'];
     }
@@ -187,7 +188,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function deleteDatabase(MariaDB $database) {
-        $url = "services/" . $this->getId() . "/gameservers/mariadbs/" . $database->getId();
+        $url = 'services/' . $this->getId() . '/gameservers/mariadbs/' . $database->getId();
         return $this->getApi()->dataDelete($url);
     }
 
@@ -197,7 +198,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function getLicenseKeys() {
-        $url = "services/" . $this->getId() . "/gameservers/license_keys";
+        $url = 'services/' . $this->getId() . '/gameservers/license_keys';
         $result = $this->getApi()->dataGet($url);
         $return = array();
         if (count($result['keys']) > 0) {
@@ -216,7 +217,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function claimLicenseKeys() {
-        $url = "services/" . $this->getId() . "/gameservers/license_keys/claim_all";
+        $url = 'services/' . $this->getId() . '/gameservers/license_keys/claim_all';
         $result = $this->getApi()->dataPost($url);
         $return = array();
         if (count($result['keys']) > 0) {
@@ -235,7 +236,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function releaseLicenseKeys() {
-        $url = "services/" . $this->getId() . "/gameservers/license_keys/release_all";
+        $url = 'services/' . $this->getId() . '/gameservers/license_keys/release_all';
         $this->getApi()->dataPost($url);
         return true;
     }
@@ -246,7 +247,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function getGames() {
-        $url = "services/" . $this->getId() . "/gameservers/games";
+        $url = 'services/' . $this->getId() . '/gameservers/games';
         return $this->getApi()->dataGet($url);
     }
 
@@ -258,7 +259,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function installGame($game, $modpack = null) {
-        $url = "services/" . $this->getId() . "/gameservers/games/install";
+        $url = 'services/' . $this->getId() . '/gameservers/games/install';
         $data =  array(
             'game' => $game,
         );
@@ -274,7 +275,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function uninstallGame($game) {
-        $url = "services/" . $this->getId() . "/gameservers/games/uninstall";
+        $url = 'services/' . $this->getId() . '/gameservers/games/uninstall';
         $this->getApi()->dataDelete($url, array(
             'game' => $game,
         ));
@@ -288,7 +289,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function startGame($game) {
-        $url = "services/" . $this->getId() . "/gameservers/games/start";
+        $url = 'services/' . $this->getId() . '/gameservers/games/start';
         $this->getApi()->dataPost($url, array(
             'game' => $game,
         ));
@@ -302,7 +303,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function changeFTPPassword($password) {
-        $url = "services/" . $this->getId() . "/gameservers/ftp/password";
+        $url = 'services/' . $this->getId() . '/gameservers/ftp/password';
         $this->getApi()->dataPost($url, array(
             'password' => $password,
         ));
@@ -316,7 +317,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function changeMySQLPassword($password) {
-        $url = "services/" . $this->getId() . "/gameservers/mysql/password";
+        $url = 'services/' . $this->getId() . '/gameservers/mysql/password';
         $this->getApi()->dataPost($url, array(
             'password' => $password,
         ));
@@ -330,7 +331,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function resetMySQLDatabase() {
-        $url = "services/" . $this->getId() . "/gameservers/mysql/reset";
+        $url = 'services/' . $this->getId() . '/gameservers/mysql/reset';
         $this->getApi()->dataPost($url);
         return true;
     }
@@ -377,7 +378,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function getAdminLogs() {
-        $url = "services/" . $this->getId() . "/gameservers/admin_logs";
+        $url = 'services/' . $this->getId() . '/gameservers/admin_logs';
         return $this->getApi()->dataGet($url);
     }
 
@@ -389,7 +390,7 @@ class Gameserver extends Service {
      * @return array
      */
     public function getStats($hours = 24) {
-        $url = "services/" . $this->getId() . "/gameservers/stats";
+        $url = 'services/' . $this->getId() . '/gameservers/stats';
         return $this->getApi()->dataGet($url, null, [
             'query' => [
                 'hours' => $hours
@@ -404,7 +405,7 @@ class Gameserver extends Service {
      * @return bool
      */
     public function sendCommand($command) {
-        $url = "services/" . $this->getId() . "/gameservers/command";
+        $url = 'services/' . $this->getId() . '/gameservers/command';
         $this->getApi()->dataPost($url, [
             'command' => $command
         ]);
@@ -420,10 +421,10 @@ class Gameserver extends Service {
      * @throws NitrapiServiceTypeNotFoundException
      */
     public function getGame($game) {
-        $class = "Nitrapi\\Services\\Gameservers\\Games\\" . ucfirst($game);
+        $class = 'Nitrapi\\Services\\Gameservers\\Games\\' . ucfirst($game);
 
         if (!class_exists($class)) {
-            throw new NitrapiServiceTypeNotFoundException("Game class " . $game . " not found");
+            throw new NitrapiServiceTypeNotFoundException('Game class ' . $game . ' not found');
         }
 
         return new $class($this);
@@ -437,11 +438,55 @@ class Gameserver extends Service {
      * @return bool
      */
     public function changeManagedRootSetting($key, $value) {
-        $url = "services/" . $this->getId() . "/gameservers/managed_root/" . $key;
+        $url = 'services/' . $this->getId() . '/gameservers/managed_root/' . $key;
         $this->getApi()->dataPost($url, [
             $key => $value
         ]);
 
         return true;
     }
+
+    /**
+     * Returns the created backups of the game server.
+     *
+     * @return array
+     * @throws NitrapiErrorException
+     */
+    public function getBackups() {
+        $url = 'services/' . $this->getId() . '/gameservers/backups';
+        $response = $this->getApi()->dataGet($url);
+
+        if (!isset($response['backups'])) {
+            throw new NitrapiErrorException('No backup have been returned from API.');
+        }
+
+        return $response['backups'];
+    }
+
+    /**
+     * Restores a backup of a gameserver image.
+     *
+     * @return array
+     */
+    public function restoreGameserverBackup($game, $number) {
+        $url = 'services/' . $this->getId() . '/gameservers/backups/gameserver';
+        return $this->getApi()->dataPost($url, [
+            'game' => $game,
+            'backup' => $number
+        ]);
+    }
+
+    /**
+     * Restores a backup of a MySQL database.
+     *
+     * @return string
+     */
+    public function restoreDatabaseBackup($database, $timestamp) {
+        $url = 'services/' . $this->getId() . '/gameservers/backups/database';
+        return $this->getApi()->dataPost($url, [
+            'database' => $database,
+            'timestamp' => $timestamp
+        ]);
+    }
+
 }
